0ee4991ccb194ff191e4f99df2fddd4122d787f6
Java-Hunter <hunt.java.job@gmail.com>
2024-07-05 16:48:35 +0530

    corrected the + sigh with desh.

--- src/main/java/org/example/DevClass.java
+++ src/main/java/org/example/DevClass.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("Dev Class+Hunter2Learn");
+        System.out.println("Dev Class Hunter2Learn");
         // test comment
         //checkin from Hunter2Learn
     }
