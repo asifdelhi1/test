9efdab34de51730724eddd3d0fa19de2cae2525a
Java-Hunter <hunt.java.job@gmail.com>
2024-07-05 17:47:27 +0530

    Added one more commit for Merge and Squash

--- src/main/java/org/example/DevClass.java
+++ src/main/java/org/example/DevClass.java
@@ -4,5 +4,6 @@ public class DevClass {
     public static void main(String[] args) {
         System.out.println("Dev Class Hunter2Learn");    
         //checkin from Hunter2Learn
+        System.out.println("Added one more commit for Merge and Squash");    
     }
 }
