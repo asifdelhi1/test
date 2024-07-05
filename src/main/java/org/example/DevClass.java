a830776355bf10084ac0a938bf8c4a964b3fd451
Java-Hunter <hunt.java.job@gmail.com>
2024-07-05 17:55:07 +0530

    Added Jira number

--- src/main/java/org/example/DevClass.java
+++ src/main/java/org/example/DevClass.java
@@ -4,6 +4,7 @@ public class DevClass {
     public static void main(String[] args) {
         System.out.println("Dev Class Hunter2Learn");    
         //checkin from Hunter2Learn
+        // Jira# 12345
         System.out.println("Added one more commit for Merge and Squash");    
     }
 }
