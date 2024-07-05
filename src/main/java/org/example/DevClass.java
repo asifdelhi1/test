bb2bd77ca61b46b56d9cc6e181405ebc6264ef83
Java-Hunter <hunt.java.job@gmail.com>
2024-07-05 14:59:28 +0530

    Update DevClass.java

--- src/main/java/org/example/DevClass.java
+++ src/main/java/org/example/DevClass.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("Dev Class");
+        System.out.println("Dev Class+Hunter2Learn");
         // test comment
+        //checkin from Hunter2Learn
     }
 }
